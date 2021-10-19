2f69c2707e8c7ffc29cfeb0ad16c23d927772911
Simon <78990353+simonfuture2@users.noreply.github.com>
2021-10-19 16:14:47 -0400

    Add files via upload

---
+++ build/precache-manifest.56db1cc50e3aa4cc742981275f773b98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c70a37bcb7827b98ad3",
+    "url": "/static/css/main.6a8b49f6.chunk.css"
+  },
+  {
+    "revision": "4c70a37bcb7827b98ad3",
+    "url": "/static/js/main.77fd9f2e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "54481131161b0e1cc84a",
+    "url": "/static/css/2.48d52519.chunk.css"
+  },
+  {
+    "revision": "54481131161b0e1cc84a",
+    "url": "/static/js/2.2df123c6.chunk.js"
+  },
+  {
+    "revision": "598eace91450566c75796f56b4988ae6",
+    "url": "/index.html"
+  }
+];
